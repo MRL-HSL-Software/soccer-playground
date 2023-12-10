b6a6d2ef7d02eb5ae26e26223d6788945e5667cd
Mobina Abforoosh <mobinaabf@gmail.com>
2023-12-10 21:39:12 +0330

    Creating a class in the header and defining the function of creating and defining points and also defining a variable as the main football field

---
+++ soccer_playground.hpp
@@ -0,0 +1,14 @@
+#ifndef SOCCER_PLAYGROUND_HPP
+#define SOCCER_PLAYGROUND_HPP
+
+#include <opencv4/opencv2/highgui/highgui.hpp>
+#include <opencv4/opencv2/imgproc/imgproc.hpp>
+
+class soccer_playground {
+    private:
+        cv::Mat football_playground;
+    public:
+        void Create();
+};
+
+#endif 
