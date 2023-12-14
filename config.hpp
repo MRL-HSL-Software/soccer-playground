b78faecb06418bbee40f6f9948ca3e5be6520872
Mobina Abforoosh <mobinaabf@gmail.com>
2023-12-14 12:22:20 +0330

    declare variables in the config using extern

--- config.hpp
+++ config.hpp
@@ -5,6 +5,27 @@
 #include <opencv4/opencv2/imgproc/imgproc.hpp>
 using namespace cv;
 
+extern double Field_length;
+extern double Field_width;
+extern double Goal_depth;
+extern double Goal_width;
+extern double Goal_height;
+extern double Goal_area_length;
+extern double Goal_area_width;
+extern double Penalty_mark_distance;
+extern double Centre_circle_diameter;
+extern double Border_strip_width;
+extern double Penalty_area_length;
+extern double Penalty_area_width;
+
+extern int scale;
+
+extern double width;
+extern double length;
+
+extern double JtoA;
+extern double EtoJ;
+extern double CtoE;
 
 
 #endif
