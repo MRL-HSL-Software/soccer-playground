4febd996e10a53c98447ccfd51ab0efe8a7f9f4d
amir <amiryn996@gmail.com>
2023-12-18 14:29:40 +0330

    header file

---
+++ main2/FieldDimensions.hpp
@@ -0,0 +1,18 @@
+#ifndef FIELD_DIMENSIONS_HPP
+#define FIELD_DIMENSIONS_HPP
+
+const int Pixel = 100;
+
+const double fieldLength = 9 * Pixel;
+const double fieldWidth = 6 * Pixel;
+const double goalDepth = 0.6 * Pixel;
+const double goalWidth = 2.6 * Pixel;
+const double goalAreaLength = 1 * Pixel;
+const double goalAreaWidth = 3 * Pixel;
+const double penaltyMarkDistance = 1.5 * Pixel;
+const double centreCircleDiameter = 1.5 * Pixel;
+const double borderStripWidth = 1 * Pixel;
+const double penaltyAreaLength = 2 * Pixel;
+const double penaltyAreaWidth = 5 * Pixel;
+
+#endif
