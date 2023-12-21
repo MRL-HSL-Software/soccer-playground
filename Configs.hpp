9d7461eceaac12e069b66351ae0e3ce94165ebd8
RamtinKosari <ramtinkosari@gmail.com>
2023-12-21 19:54:25 +0330

    [#REFACTOR] Update Configs File

---
+++ Configs.hpp
@@ -0,0 +1,67 @@
+# ifndef __MRL__
+    # pragma message \
+        "This Project Must be Included in MRL Projects Only. Use MRL CMake Files"
+# else
+    # define __PROJECT_VERSION__ "1.0"
+# endif // __MRL_HSL
+
+# ifndef __MRL_SOCCER_PLAYGROUND_CONFIGS__
+    /**
+     * @file Configs.hpp
+     * @author Mobina Abforoosh (mobinaabf@gmail.com)
+     * @author Ramtin Kosari (ramtinkosari@gmail.com)
+     * @author Parsa Karbasi (ijackgonjishke@gmail.com)
+     * @author Alireza Mortezaei (alirezamortezaei50@gmail.com)
+     * @author Amir Yusefian
+     * @author Erfan Zarabadi
+     * @brief MRL HSL Soccer Filed Preview with OpenCV
+     * @version 1.0.0
+     * @date 2023-12-21
+     */
+    # define __MRL_SOCCER_PLAYGROUND_CONFIGS__
+    //-- Define Playground Dimensions
+# endif // __MRL_SOCCER_PLAYGROUND_CONFIGS__
+
+
+// // am code
+
+// #define BASE_INDEX 80.0
+// #define LINE_WIDTH 1.0
+// #define MAIN_RECT_WIDTH 9.0
+// #define MAIN_RECT_HEIGHT 6.0
+// #define MAIN_RECT_MARGIN 1.0
+// #define MIDDLE_CIRCLE_RADIUS 1.5
+// #define LARG_GOAL_RECT_HEIGHT 5.0
+// #define LARG_GOAL_RECT_WIDTH 2.0
+// #define SMALL_GOAL_RECT_HEIGHT 3.0
+// #define SMALL_GOAL_RECT_WIDTH 1.0
+// #define GOAL_HEIGHT 2.6
+// #define GOAL_WIDTH 0.6
+// #define PENALTY_MARK_DISTANCE 1.5
+// #define PENALTY_MARK_SIZE 0.05
+
+
+
+// // mobina
+
+// #define FIELD_LENGTH 9.0
+// #define FIELD_WIDTH 6.0
+// #define GOAL_DEPTH 0.6
+// #define GOAL_WIDTH 2.6
+// #define GOAL_HEIGHT 1.2
+// #define GOAL_AREA_LENGTH 1.0
+// #define GOAL_AREA_WIDTH 3.0
+// #define PENALTY_MARK_DISTANCE 1.5
+// #define CENTER_CIRCLE_DIAMETER 1.5
+// #define BORDER_STRIP_WIDTH 1.0
+// #define PENALTY_AREA_LENGTH 2.0
+// #define PENALTY_AREA_WIDTH 5.0
+
+// #define SCALE 70
+
+// #define WIDTH 
+// #define LENGTH 
+
+// #define JTOA 
+// #define ETOJ 
+// #define CTOE 
---
+++ main.cpp
@@ -0,0 +1,6 @@
+# include <iostream>
+// # include "Configs.hpp"
+
+int main() {
+    std::cout << __MRL__ << std::endl;
+}
